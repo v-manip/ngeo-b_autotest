abdc44cabe164f69b7c7edab1b1d6d9c5cf4ed87
Stephan Meissl <stephan@meissl.name>
2013-05-31 13:30:19 +0200

    Chaning default logging level to INFO.

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/ngeob/autotest/static/'
+STATIC_ROOT = join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -204,16 +204,16 @@ LOGGING = {
     'handlers': {
         'eoxserver_file': {
             'level': 'DEBUG',
-            'class': 'logging.FileHandler',
+            'class': 'logging.WatchedFileHandler',
             'filename': join(PROJECT_DIR, 'logs', 'eoxserver.log'),
-            'formatter': 'verbose',
+            'formatter': 'verbose' if DEBUG else 'simple',
             'filters': [],
         },
         'ngeo_file': {
             'level': 'DEBUG',
-            'class': 'logging.FileHandler',
+            'class': 'logging.WatchedFileHandler',
             'filename': join(PROJECT_DIR, 'logs', 'ngeo.log'),
-            'formatter': 'verbose',
+            'formatter': 'verbose' if DEBUG else 'simple',
             'filters': [],
         }
     },
