e56175b82b130294c90bb0050ae627e91228a048
Daniel Santillan <daniel.santillan@eox.at>
2013-08-21 17:00:05 +0200

    Modified creat_chunks script to modify browse reports in order to avoid any time overlapping, creates a time intervall in the csv for each browse report uniquely identifiying them for export, delete and import methods that will be tested

--- integration_tests/image2012/console_performance/create_chunks.py
+++ integration_tests/image2012/console_performance/create_chunks.py
@@ -3,54 +3,143 @@ import argparse
 import time
 import os
 from time import gmtime, strptime, strftime
+import itertools, collections
+
+import pdb
+
+from datetime import datetime, timedelta
 from xml.dom.minidom import parse, parseString
+
+
+def consume(iterator, n):
+    collections.deque(itertools.islice(iterator, n))
 
 
 #argument specification for script
 parser = argparse.ArgumentParser(description='Script which allows to create a csv which separates browse reports in equaly sized chunks of times')
 
 parser.add_argument('--create', nargs=2,
-                   help='Pass arguments with path to browse reports, browsecount and output csv filepath')
+                   help='Pass arguments with path to browse reports and output csv filepath')
 
-parser.add_argument('chunks', default=50, type=int,
-                   help='an integer for the chunk size')
+parser.add_argument('--output', nargs=1, default='modifiedBrowses/',
+                   help='Pass path to where output shall be saved, default is modifiedBrowses/')
 
 
-def createChunks(browsePath, output, browsecount):
-	f = open(output,'w')
+def createChunks(browsePath, outputfile, output):
+	f = open(outputfile,'w')
 	#2013-08-03T10:07:24+00:00
 	dateformat = "%Y-%m-%dT%H:%M:%S+00:00"
 	os.system("echo ======================================================================")
 	os.system("echo ========= Creating chunks containing equal number of browses =========")
 	os.system("echo ======================================================================")
 	browses = {}
+	browsereports = []
 
 	#collect all layerids and their start and endtimes
-	for files in os.listdir(browsePath):
-		xmlfile = parse(browsePath + files)
+	for filename in os.listdir(browsePath):
+		export = output + filename
+		filename = browsePath + filename
+		xmlfile = parse(filename)
 		typeid = xmlfile.getElementsByTagName('rep:browseType')[0].firstChild.wholeText
-		start = strptime(xmlfile.getElementsByTagName('rep:startTime')[0].firstChild.wholeText,dateformat)
-		end = strptime(xmlfile.getElementsByTagName('rep:endTime')[0].firstChild.wholeText,dateformat)
-		if (typeid in browses):
-			browses[typeid].append((start,end))
-		else:
-			browses[typeid] = [(start,end)]
+		start = datetime.strptime(xmlfile.getElementsByTagName('rep:startTime')[0].firstChild.wholeText, dateformat)
+		end = datetime.strptime(xmlfile.getElementsByTagName('rep:endTime')[0].firstChild.wholeText, dateformat)
+		
+		browses.setdefault(typeid, []).append([start, end, xmlfile])
+		browsereports.append((xmlfile, export))
 
-	#sort by times and create chunks containing a defined number of browses
+	#sort by times, modify browse report times so they dont overlap 
+	#and write start and entime for each browsereport
 	for layers in browses:
 		browses[layers].sort()
-		for i in xrange(0,len(browses[layers]),browsecount):
-			next_i = (i+browsecount-1) if ((i+browsecount-1)<len(browses[layers])) else (len(browses[layers])-1)
-			out_start = time.strftime(dateformat, browses[layers][i][0])
-			out_end = time.strftime(dateformat, browses[layers][next_i][1])
-			f.write("%s,%s,%s\n"%(layers,out_start,out_end))
+
+		iterator = enumerate(browses[layers]).__iter__()
+
+		for i, (start, end, xmlfile) in iterator:
+
+			
+			next_i = (i+1) if ((i+1)<len(browses[layers])) else -1
+			amount_overlap = 0
+
+			if next_i > -1:
+				#while end_time is bigger then start_time of the next browsereport
+				while end >= browses[layers][next_i][0]: 
+					#count how many overlap
+					amount_overlap += 1
+
+					#check if we reached last element in list
+					if( next_i+1 >= len(browses[layers]) ):
+						break
+
+					#increment next_i to compare next browse report times in next loop
+					next_i += 1
+
+				if (amount_overlap > 0):
+					min_start = browses[layers][i][0] 
+					max_end = browses[layers][next_i][1]
+					#time_interval = ((max_end - min_start) + timedelta(seconds=10)) /amount_overlap
+					time_interval = (max_end - min_start) /amount_overlap
+				else:
+					min_start = browses[layers][i][0] 
+					max_end = browses[layers][i][1]
+					time_interval = (max_end - min_start)
 
 
+				multiplier = 0
+				if amount_overlap > 0:
+					for j in xrange(i, next_i+1):
 
+						browses[layers][j][0] = (min_start + time_interval*multiplier) + timedelta(seconds=1)
+						multiplier = multiplier+1
+						browses[layers][j][1] = (min_start + time_interval*multiplier)
+
+						#pdb.set_trace()
+
+						out_start = browses[layers][j][0].strftime(dateformat)
+						out_end = browses[layers][j][1].strftime(dateformat)
+
+						#Modify browse reports with new start end times
+						browses[layers][j][2].getElementsByTagName('rep:startTime')[0].firstChild.replaceWholeText(out_start)
+						browses[layers][j][2].getElementsByTagName('rep:endTime')[0].firstChild.replaceWholeText(out_end)
+
+						#write layerid start and end times of browse to the csv
+						f.write("%s,%s,%s\n"%(layers,out_start,out_end))
+
+					#browse reports already analyses (time overlapped) can be jumped over					
+					consume(iterator,amount_overlap)
+
+				else:
+					browses[layers][i][0] = min_start
+					browses[layers][i][1] = max_end
+
+					out_start = browses[layers][i][0].strftime(dateformat)
+					out_end = browses[layers][i][1].strftime(dateformat)
+
+					#Modify browse reports with new start end times
+					browses[layers][i][2].getElementsByTagName('rep:startTime')[0].firstChild.replaceWholeText(out_start)
+					browses[layers][i][2].getElementsByTagName('rep:endTime')[0].firstChild.replaceWholeText(out_end)
+
+					#write layerid start and end times of browse to the csv
+					f.write("%s,%s,%s\n"%(layers,out_start,out_end))
+
+	
+	for dom, filename in browsereports:
+		with open(filename, "w+") as xmlf:
+			xmlf.write(dom.toxml("utf-8"))
+
+
+	#Check there are actually no overlapping times
+	for layers in browses:
+		browses[layers].sort()
+
+		for i, (start, end, xmlfile) in enumerate(browses[layers]):
+			next_i = (i+1) if ((i+1)<len(browses[layers])) else -1
+			if next_i > -1:
+				if end >= browses[layers][next_i][0]: 
+					print "OVERLAP FOUND!!"
 
 
 args = parser.parse_args()
 
 if (args.create):
-	createChunks(args.create[0], args.create[1], args.chunks)
+	createChunks(args.create[0], args.create[1], args.output[0])
 
