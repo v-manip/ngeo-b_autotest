744926dbf39b9f6d2321ab992d6bc6ba7795d6b2
Stephan Meissl <stephan@meissl.name>
2013-10-07 14:37:42 +0200

    Updating install script to latest changes in PostGIS and PROJ packages. Adding documentation to add browse layers script.

--- integration_tests/add_browse_layers.sh
+++ integration_tests/add_browse_layers.sh
@@ -34,7 +34,18 @@
 #
 # Browse Layers are provided in a separate configuration file following this
 # structure:
-#<LAYER_NAME> <BROWSE_TYPE> <LOWEST_MAP_LEVEL> <HIGHEST_MAP_LEVEL>
+#<LAYER_NAME> <BROWSE_TYPE> <LOWEST_MAP_LEVEL> <HIGHEST_MAP_LEVEL> <grid> <r_band> <g_band> <b_band> <radiometric_interval_min> <radiometric_interval_max>
+#
+# Note that the first five parameters are mandatory while the second five are 
+# optional.
+#
+# Note that usually 0 is used for the <LOWEST_MAP_LEVEL>. Level 10 which is 
+# around 75m/px is a good candidate for <HIGHEST_MAP_LEVEL>.
+#
+# Choices for <grid> are either 3857 or 4326 corresponding to 
+# "urn:ogc:def:wkss:OGC:1.0:GoogleMapsCompatible" or 
+# "urn:ogc:def:wkss:OGC:1.0:GoogleCRS84Quad" respectively.
+#
 
 # Running:
 # =======
