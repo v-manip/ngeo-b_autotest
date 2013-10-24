5cbe512ce816bf7c40be6f320783ab49c494c4bb
Stephan Meissl <stephan@meissl.name>
2013-10-24 14:51:23 +0200

    Adding upload switch to load_test_data.sh script.

--- integration_tests/load_test_data.sh
+++ integration_tests/load_test_data.sh
@@ -42,31 +42,35 @@
 ################################################################################
 
 url=$1
+upload=$2
 
 [ "$1" ] || url="http://localhost:3080"
 
+if [ "$2" ] ; then
+    echo "Sending browse images to: $url"
+
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100722_213840.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100731_103315.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100807_101327.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100807_101327_new.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100813_102453.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100719_101023.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100722_101601.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100725_102231.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100719_105257.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100719_213253.jpg "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100722_101606.jpg "$url"/store/
+     
+    curl --digest -u test:eiNoo7ae -T ../data/test_data/ASA_WSM_1PNDPA20050331_075939_000000552036_00035_16121_0775.tif "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced_nogeo.tif "$url"/store/
+    curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced.tif "$url"/store/
+
+    curl --digest -u test:eiNoo7ae -T ../data/aiv_test_data/NGEO-FEED-VTC-0040.jpg "$url"/store/
+
+    curl --digest -u test:eiNoo7ae -T ../data/feed_test_data/quick-look.png "$url"/store/
+fi
+
 echo "Sending browse reports to: $url"
-
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100722_213840.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100731_103315.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100807_101327.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100807_101327_new.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_IM__0P_20100813_102453.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100719_101023.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100722_101601.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ASA_WS__0P_20100725_102231.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100719_105257.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100719_213253.jpg "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/reference_test_data/ATS_TOA_1P_20100722_101606.jpg "$url"/store/
- 
-curl --digest -u test:eiNoo7ae -T ../data/test_data/ASA_WSM_1PNDPA20050331_075939_000000552036_00035_16121_0775.tif "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced_nogeo.tif "$url"/store/
-curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced.tif "$url"/store/
-
-curl --digest -u test:eiNoo7ae -T ../data/aiv_test_data/NGEO-FEED-VTC-0040.jpg "$url"/store/
-
-curl --digest -u test:eiNoo7ae -T ../data/feed_test_data/quick-look.png "$url"/store/
-
 
 curl -d @../data/reference_test_data/browseReport_ASA_IM__0P_20100722_213840.xml "$url"/browse/ingest
 curl -d @../data/reference_test_data/browseReport_ASA_IM__0P_20100731_103315.xml "$url"/browse/ingest
@@ -86,10 +90,9 @@ curl -d @../data/aiv_test_data/BrowseReport.xml "$url"/browse/ingest
 
 curl -d @../data/feed_test_data/BrowseReport.xml "$url"/browse/ingest
 
-# Add configuration to MapCache before execution of the below ingestions.
-#curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.tif "$url"/store/
-#curl -d @../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed_NO_BANDS.xml "$url"/browse/ingest
-#curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.tif "$url"/store/
-#curl -d @../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.xml "$url"/browse/ingest
-#curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced.tif "$url"/store/
-#curl -d @../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced_GOOGLE_MERCATOR.xml "$url"/browse/ingest
+curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.tif "$url"/store/
+curl -d @../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed_NO_BANDS.xml "$url"/browse/ingest
+curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.tif "$url"/store/
+curl -d @../data/test_data/MER_FRS_1PNPDE20060816_090929_000001972050_00222_23322_0058_uint16_reduced_compressed.xml "$url"/browse/ingest
+curl --digest -u test:eiNoo7ae -T ../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced.tif "$url"/store/
+curl -d @../data/test_data/MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_RGB_reduced_GOOGLE_MERCATOR.xml "$url"/browse/ingest
