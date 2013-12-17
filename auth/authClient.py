ae8861881ccae4e9972115bb553570c9df56c623
szabi <szabolcs.rimanoczy@berner-mattner.at>
2013-12-17 14:23:35 +0100

    ->Added policy decision cache to Client. ->Cache parameters and client-server connection parameters are now configurable in eoxserver.conf. ->Server reads user rights from file (policy.xml)

--- auth/authClient.py
+++ auth/authClient.py
@@ -3,94 +3,209 @@
 import httplib
 from django.http import HttpResponse
 from xml.etree import ElementTree
-                                 
-#-------------------------------------------------------------------------------
-# AuthorizerClientMiddleware
-#-------------------------------------------------------------------------------    
-class AuthorizerClientMiddleware(object):  
-        
-    def process_request(self, request):  
+from eoxserver.core.config import get_eoxserver_config
+import memcache
+import logging
     
-          #Get user attributes from the request
-          surname = request.META['surname'] 
-          commonName = request.META['commonName']
-          userId = request.META['uid']
-          browseLayerId = request.GET.get("BrowseLayer", "empty")
-          timePeriod = request.GET.get("Time", "empty")
-          error = ['']
-
-          authorizerServerInterface = AuthorizerServerInterface()
-          
-          authorized = authorizerServerInterface.isRequestAuthorized(userId, browseLayerId, timePeriod, error)
-          if authorized:
-            #OK - let the request through
-            return None
-          else:
-            #NOK - deny request
-            response = HttpResponse()  
-            self.buildNotAuthorizedResponse(response, surname, commonName, userId, error)
-            return response
-
-    #-------------------------------------------------------------------------------
-    # buildNotAuthorizedResponse
-    #-------------------------------------------------------------------------------      
-    def buildNotAuthorizedResponse(self, response, surname, commonName, userId, error):
-      response.status_code = 401
-      response.write("<b>%s</b> NOT AUTHORIZED" % (str(response.status_code)))
-      response.write("<p>User %s %s (userId: %s) is not authorized to access this content.</p>" % (surname, commonName, userId))                
-      response.write("<p>%s</p>" % (error[0]))                
-      return response
-    
-    
-#-------------------------------------------------------------------------------
-# AuthorizerServerInterface
-#-------------------------------------------------------------------------------    
-class AuthorizerServerInterface(object):
-    
-    def isRequestAuthorized(self, userId, browseLayerId, timePeriod, error):
-      
-      #TODO: should be configurable from cfg file?
-      authorizerServerBaseUrl = "localhost:8001"
-    
-      try:      
-        #Set up connection to authorizer server   
-        authServerConnection = httplib.HTTPConnection(authorizerServerBaseUrl)
+def enum(**enums):
+  return type('Enum', (), enums)
   
-        #Prepare the URL parameters
-        url = "/ngEOWebServer/BrwsAuthorizationCheck?UserId=%s&BrowseLayerId=%s&TimePeriod=%s" % (userId, browseLayerId, timePeriod)
+Decision = enum(DONTKNOW=0, AUTHORIZED=1, DENIED=2) 
+
+logger = logging.getLogger(__name__)
+                             
+################################################################################
+# AuthorizerClientMiddleware
+################################################################################    
+class AuthorizerClientMiddleware(object):  
+           
+  #-------------------------------------------------------------------------------
+  # process_request (the main Middleware function)
+  #-------------------------------------------------------------------------------      
+  def process_request(self, request):  
+  
+        #Get user attributes from the request
+        userId = request.META['uid']
         
-        #Send request
-        authServerConnection.request("GET", url)
-        resp = authServerConnection.getresponse()
+        #TODO: the browseLayerId must be parsed in future by a specialized eoxserver decoder class
+        browseLayerId = request.GET.get("BrowseLayer", "empty")
         
-        #Check the response status
-        if resp.status != 200:
-          raise Exception("server response: %d" % resp.status)
-          
-        #Parse the XML Response and get the elements that we are interested in     
-        responseBody = resp.read()
-        rootElem = ElementTree.XML(responseBody)    
-   
-        namespace = '{http://ngeo.eo.esa.int/schema/webserver}'
-        responseCodeElem = rootElem.find("%sResponseCode" % namespace)     
-        if responseCodeElem is None:
-          raise Exception("element 'ResponseCode' not found in XML-Response of authorizer server")
-          
-        #Read the policy decision
-        if responseCodeElem.text == "AUTHORIZED":
-          authorized = True
-        elif responseCodeElem.text == "NOT_AUTHORIZED":
-          authorized = False
-        else:
-          raise Exception("element 'ResponseCode' does not contain a valid value.")
-         
-        exceptionElem = rootElem.find("%sExceptionElement" % namespace) 
-        if exceptionElem is not None:
-          error[0] = exceptionElem.text
-        
-      except Exception, e:
-        authorized = False 
-        error[0] = "Error while communication with authorizer server: %s" % str(e) 
+        decision = Decision.DONTKNOW
+        error = ['']
                  
-      return authorized
-        
+        #See if we find the decision in the cache         
+        cachedPD = PolicyDecisionCache().getDecision(userId, browseLayerId)
+        if(cachedPD.decision == Decision.DONTKNOW):
+          #Ask the server         
+          decision = AuthorizerServerInterface().isRequestAuthorized(userId, browseLayerId, error)
+          if not error[0]:
+            #Save the decision to our cache
+            PolicyDecisionCache().addDecision(userId, browseLayerId, decision)
+        else:
+          #Apply the cached decision
+          decision = cachedPD.decision
+         
+        if (decision == Decision.AUTHORIZED):
+           #OK - let the request through
+           return None
+        else:
+           #NOK - deny request
+           response = HttpResponse()  
+           self.buildDeniedResponse(response, userId, error)
+           return response
+
+  #-------------------------------------------------------------------------------
+  # buildDeniedResponse
+  #-------------------------------------------------------------------------------      
+  def buildDeniedResponse(self, response, userId, error):
+    response.status_code = 401
+    response.write("<b>%s</b> NOT AUTHORIZED" % (str(response.status_code)))
+    response.write("<p>User %s is not authorized to access this content.</p>" % (userId))                
+    response.write("<p>%s</p>" % (error[0]))                
+    return response  
+
+    
+################################################################################
+# AuthorizerServerInterface
+################################################################################    
+class AuthorizerServerInterface(object):
+
+  #-------------------------------------------------------------------------------
+  # Connect
+  #-------------------------------------------------------------------------------
+  def Connect(self):
+    authServerAddr = ConfigReader().getAuthServerAddress()
+    
+    #Set up connection to authorizer server and return the connection object 
+    return httplib.HTTPConnection(authServerAddr)
+
+     
+  #-------------------------------------------------------------------------------
+  # isRequestAuthorized
+  #-------------------------------------------------------------------------------
+  def isRequestAuthorized(self, userId, browseLayerId, error):
+   
+    #Prepare the URL parameters
+    url = "/ngEOWebServer/BrwsAuthorizationCheck?UserId=%s&BrowseLayerId=%s" % (userId, browseLayerId)
+    
+    try:
+      conn = self.Connect()
+
+      #Send request
+      conn.request("GET", url)
+      resp = conn.getresponse()
+      
+      #Check the response status
+      if resp.status != 200:
+        raise Exception("server response: %d" % resp.status)
+      
+      responseBody = resp.read()  
+      decision = self.ParseServerResponse(responseBody, error)
+            
+    except Exception, e:
+      decision = Decision.DENIED
+      error[0] = "Error while communication with authorizer server: %s" % str(e) 
+               
+    return decision
+
+
+#-------------------------------------------------------------------------------
+# ParseServerResponse
+#-------------------------------------------------------------------------------
+  def ParseServerResponse(self, responseBody, error):
+  
+    #Parse the XML Response and get the elements that we are interested in     
+    rootElem = ElementTree.XML(responseBody)    
+  
+    namespace = '{http://ngeo.eo.esa.int/schema/webserver}'
+    responseCodeElem = rootElem.find("%sResponseCode" % namespace)     
+    
+    if responseCodeElem is None:
+      raise Exception("element 'ResponseCode' not found in XML-Response of authorizer server")
+      
+    #Read the policy decision
+    if responseCodeElem.text == "AUTHORIZED":
+      decision = Decision.AUTHORIZED
+    elif responseCodeElem.text == "NOT_AUTHORIZED":
+      decision = Decision.DENIED
+    else:
+      raise Exception("element 'ResponseCode' does not contain a valid value.")
+     
+    exceptionElem = rootElem.find("%sExceptionElement" % namespace) 
+    if exceptionElem is not None:
+      error[0] = exceptionElem.text
+
+    return decision
+
+     
+################################################################################
+# PolicyDecisionCache
+################################################################################    
+class PolicyDecisionCache(object):
+
+  cache = None 
+  cacheTimeout = 0
+  
+  def __init__(self):
+    self.cacheTimeout = ConfigReader().getCacheTimeout() 
+    self.cache = memcache.Client([ConfigReader().getCacheServerAddress()])
+     
+  #-------------------------------------------------------------------------------
+  # addDecision
+  #-------------------------------------------------------------------------------  
+  def addDecision(self, userId, browseLayerId, decision):
+    #Create a new PolicyDecision object
+    pd = PolicyDecision()
+    pd.userId = userId   
+    pd.browseLayerId = browseLayerId 
+    pd.decision = decision
+
+    #Create a key for it
+    key = self.makeKey(userId, browseLayerId)
+
+    #Save the PolicyDecision in the cache 
+    self.cache.set(key, pd, self.cacheTimeout)
+                
+  #-------------------------------------------------------------------------------
+  # getDecision
+  #-------------------------------------------------------------------------------  
+  def getDecision(self, userId, browseLayerId):
+    key = self.makeKey(userId, browseLayerId)
+    pd = self.cache.get(key)
+    if (pd == None):
+      #Return an empty PolicyDecision to signalize that it does not exist in the cache
+      pd = PolicyDecision()
+      pd.decision = Decision.DONTKNOW
+     
+    return pd 
+    
+  #-------------------------------------------------------------------------------
+  # makeKey
+  #-------------------------------------------------------------------------------  
+  def makeKey(self, userId, browseLayerId):
+    return ("%s%s" % (userId.encode('utf-8'), browseLayerId.encode('utf-8')))
+
+ 
+################################################################################
+# PolicyDecision
+################################################################################    
+class PolicyDecision:
+  userId = ""
+  browseLayerId = ""
+  decision = Decision.DONTKNOW
+  
+  
+
+################################################################################
+# ConfigReader
+################################################################################
+class ConfigReader(object):
+
+  def getAuthServerAddress(self):
+      return get_eoxserver_config().get("services.auth.ngeo", "authserver_addr")
+
+  def getCacheServerAddress(self):
+      return get_eoxserver_config().get("services.auth.ngeo", "cacheserver_addr")
+
+  def getCacheTimeout(self):
+      return get_eoxserver_config().getint("services.auth.ngeo", "cache_timeout")
--- auth/authServer.py
+++ auth/authServer.py
@@ -1,92 +1,122 @@
 #!/usr/bin/env python
 
 from wsgiref import simple_server
+from xml.etree import ElementTree
 import cgi
-
-#------------------------------------------------------------------------------
-# RequestHandler
-#------------------------------------------------------------------------------
-def RequestHandler(env, response):
-
-    print("\nIncoming request:")
-    status = "200 OK"
-    statusNOK = "500 Internal Server Error"
-     
-    authorizedMessage =   """<?xml version="1.0" encoding="UTF-8"?>
-                             <ws:BrwsAuthorizationCheckResponse xsi:schemaLocation="http://ngeo.eo.esa.int/schema/webserver ngEO-EICD-WS.xsd" xmlns:ws="http://ngeo.eo.esa.int/schema/webserver" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-                                <ws:ResponseCode>AUTHORIZED</ws:ResponseCode>
-                             </ws:BrwsAuthorizationCheckResponse>"""
-
-    deniedMessage =       """<?xml version="1.0" encoding="UTF-8"?>
-                             <wsBrwsAuthorizationCheckResponse xsi:schemaLocation="http://ngeo.eo.esa.int/schema/webserver ngEO-EICD-WS.xsd" xmlns:ws="http://ngeo.eo.esa.int/schema/webserver" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-                                <ws:ResponseCode>NOT_AUTHORIZED</ws:ResponseCode>
-                             </wsBrwsAuthorizationCheckResponse>"""
-
-    try:
-      #get the query string parameters from the url
-      queryParams = GetQueryParams(env)
-  
-      #check if the user is authorized to access the content based on the query parameters 
-      authorized = MakePolicyDecision(queryParams)
+import os
       
-      if authorized:
-        print("  -> Request authorized\n")
-        responseBody = authorizedMessage
-      else:
-        print("  -> Request denied\n")
+PORT = 8001
+POLICYFILE = "policy.xml"
+
+################################################################################
+# AuthServer
+################################################################################
+class AuthServer(object):
+
+  #------------------------------------------------------------------------------
+  # RequestHandler
+  #------------------------------------------------------------------------------
+  def RequestHandler(self, env, response):
+  
+      print("\n--------------------------\nIncoming request:")
+      status = "200 OK"
+      statusNOK = "500 Internal Server Error"
+       
+      authorizedMessage =   """<?xml version="1.0" encoding="UTF-8"?>
+                               <ws:BrwsAuthorizationCheckResponse xsi:schemaLocation="http://ngeo.eo.esa.int/schema/webserver ngEO-EICD-WS.xsd" xmlns:ws="http://ngeo.eo.esa.int/schema/webserver" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                                  <ws:ResponseCode>AUTHORIZED</ws:ResponseCode>
+                               </ws:BrwsAuthorizationCheckResponse>"""
+  
+      deniedMessage =       """<?xml version="1.0" encoding="UTF-8"?>
+                               <wsBrwsAuthorizationCheckResponse xsi:schemaLocation="http://ngeo.eo.esa.int/schema/webserver ngEO-EICD-WS.xsd" xmlns:ws="http://ngeo.eo.esa.int/schema/webserver" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                                  <ws:ResponseCode>NOT_AUTHORIZED</ws:ResponseCode>
+                               </wsBrwsAuthorizationCheckResponse>"""
+  
+      try:
+        #get the query string parameters from the url
+        queryParams = self.GetQueryParams(env)
+    
+        #check if the user is authorized to access the content based on the query parameters 
+        authorized = self.MakePolicyDecision(queryParams)
+        
+        if authorized:
+          print("  -> Request authorized\n")
+          responseBody = authorizedMessage
+        else:
+          print("  -> Request denied\n")
+          responseBody = deniedMessage
+      except Exception, e:
+        print("%s" % str(e))
         responseBody = deniedMessage
-    except Exception, e:
-      print("%s" % str(e))
-      responseBody = deniedMessage
-      status = statusNOK
-    
-    response(status, [('Content-type', 'text/xml')])
-    
-    return ["%s" % responseBody]
-    
-
-#------------------------------------------------------------------------------
-# GetQueryParams
-#------------------------------------------------------------------------------
-def GetQueryParams(env):
-  qs = env.get('QUERY_STRING')
-  if qs:
-     return dict((k, v)
-            for k, v in cgi.parse_qsl(qs))
-  else:
-     return {}
-
-#------------------------------------------------------------------------------
-# MakePolicyDecision
-#------------------------------------------------------------------------------
-def MakePolicyDecision(queryParams):
-    authorized = False
-    
-    browseLayerId = queryParams.get('BrowseLayerId', "")
-    userId = queryParams.get('UserId', "")
-    timePeriod = queryParams.get('TimePeriod', "")
-    
-    print("  UserId: [%s]" % userId)
-    print("  BrowseLayerId: [%s]" % browseLayerId)
-    print("  TimePeriod: [%s]" % timePeriod)
-    
-    #Test policy rule
-    if (userId == "musterfrau") and \
-       (browseLayerId == "SAR_L1_BROW"):
-      authorized = True
+        status = statusNOK
+      
+      response(status, [('Content-type', 'text/xml')])
+      
+      return ["%s" % responseBody]
+      
+  
+  #------------------------------------------------------------------------------
+  # GetQueryParams
+  #------------------------------------------------------------------------------
+  def GetQueryParams(self, env):
+    qs = env.get('QUERY_STRING')
+    if qs:
+       return dict((k, v)
+              for k, v in cgi.parse_qsl(qs))
     else:
+       return {}
+  
+  #------------------------------------------------------------------------------
+  # MakePolicyDecision
+  #------------------------------------------------------------------------------
+  def MakePolicyDecision(self, queryParams):
       authorized = False
-    
-    return authorized
+      
+      browseLayerId = queryParams.get('BrowseLayerId', "")
+      userId = queryParams.get('UserId', "")
+      
+      print("  UserId: [%s]" % userId)
+      print("  BrowseLayerId: [%s]" % browseLayerId)
+      
+      rights = PolicyReader().GetUserRights(userId)
+      if browseLayerId in rights:
+        authorized = True
+      else:
+        authorized = False
+      
+      return authorized
 
+################################################################################
+# PolicyReader
+################################################################################
+class PolicyReader(object):
 
-#------------------------------------------------------------------------------
+  def GetUserRights(self, userId):
+    rights = []
+  
+    try:
+      #read rights of the user from the policy file
+      policyFile = "%s/%s" % (os.path.dirname( __file__ ), POLICYFILE)
+      root = ElementTree.parse(policyFile)
+      
+      for user in root.findall('User'):      
+        if (user.attrib["Id"] == userId):
+          for right in user.findall('Rights/BrowseLayerId'):
+            rights.append(right.text)
+            
+    except Exception, e:
+      print "ERROR while reading policy.xml: %s" % str(e) 
+          
+    return rights
+
+       
+################################################################################
 # Main
-#------------------------------------------------------------------------------       
+################################################################################
+
 # Set up the server and bind the request handler function    
-port = 8001
-server = simple_server.make_server('', port, RequestHandler)
-print "Test authorizer server running on port %d... (Ctrl-C to exit)" % port
+server = simple_server.make_server('', PORT, AuthServer().RequestHandler)
+print "Authorizer server running on port %d... (Ctrl-C to exit)" % PORT
 
 # Run the server until the process is killed
 server.serve_forever()
